a5a48af0bb15d099bab2ad9e186399c0243f2661
Alexander <kvendingoldo@yandex.ru>
2024-10-01 18:39:30 +0200

    feat: initial implementation
    Signed-off-by: Alexander <kvendingoldo@yandex.ru>

---
+++ src/tenv-action.sh
@@ -0,0 +1,23 @@
+#
+# Copyright 2024 tofuutils authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#!/bin/bash
+set -e
+
+TOOL_NAME="${INPUT_TOOL_NAME}"
+TOOL_VERSION="${INPUT_TOOL_VERSION}"
+GITHUB_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+tenv ${TOOL_NAME} install ${TOOL_VERSION}
